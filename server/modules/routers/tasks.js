7bd3e13cddf9eff777d6b4f1f72c626bfe4939df
Dev Jana <dev@primeacademy.io>
2018-08-13 10:13:44 -0500

    all routes and basic FE working

--- server/modules/routers/tasks.js
+++ server/modules/routers/tasks.js
@@ -2,17 +2,32 @@
 const express = require( 'express' );
 const router = express.Router();
 
-// Create
+/// - test before db - ///
+let tasks = [];
 
+// Create
+router.post( '/', ( req, res )=>{
+    console.log( 'in POST:', req.body );
+    tasks.push( req.body );
+    res.send( 'roar' );
+}) // end POST
 // Read
 router.get( '/', ( req, res )=>{
     console.log( 'in GET hit' );
-    res.send( 'sqeak' );
+    res.send( tasks );
 }) // end GET
 
 // Update
+router.put( '/', ( req, res )=>{
+    console.log( 'updating:', req.body );
+    res.send( 'purrrrrrrrrrrrrrrrr' );
+}) // end PUT
 
 // Delete
+router.delete( '/', ( req, res )=>{
+    console.log( 'deleting:', req.query );
+    res.send( 'kacaw' );
+}) // end delete
 
 // exports
 module.exports = router;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,79 @@
 const myApp = angular.module( 'myApp', [] );
 
 myApp.controller( 'TasksController', function( $http ){
-    console.log( 'NG' );
+    const vm = this;
+    vm.tasks = [];
+
+    vm.addTask = function(){
+        console.log( 'adding task:', vm.nameIn );
+        let objectToSend = {
+            name: vm.nameIn,
+            complete: false
+        } // end objectToSend
+        console.log( 'sending:', objectToSend );
+        // $http POST call to /tasks with this data
+        $http({
+            method: 'POST',
+            url: '/tasks',
+            data: objectToSend
+        }).then( function( response ){
+            console.log( 'back from server with:', response );
+            // update DOM
+            vm.getTasks();
+            // clear inputs
+            vm.nameIn = '';
+        }).catch( function( err ){
+            alert( 'could not add task' );
+            console.log( 'error adding task:', err );
+        }) // end $http
+    } // end addTask
+
+    vm.deleteTask = function( index ){
+        console.log( 'deleting task:', vm.tasks[ index ] );
+        $http({
+            method: 'DELETE',
+            url: '/tasks?id=' + index
+        }).then( function( response ){
+            console.log( 'back from server with:', response );
+            vm.getTasks();
+        }).catch( function( err ){
+            console.log( 'error deleting task:', err );
+            alert( 'problem!' );
+        }) // end $http
+    } // end deleteTask
+
+    vm.getTasks = function(){
+        $http({
+            method: 'GET',
+            url: '/tasks'
+        }).then( function ( response ){
+            console.log( 'back from server:', response.data );
+            vm.tasks = response.data;
+        }).catch( function( err ){
+            console.log( 'error getting tasks:', err );
+            alert( 'unable to retrieve tasks' );
+        }) // end $http
+    } // end getTasks
+
+    vm.toggleTask = function( index ){
+        console.log( 'in toggleTask:', vm.tasks[ index ] );
+        /// - test toggle - ///
+        vm.tasks[ index ].complete = !vm.tasks[ index ].complete;
+        console.log( 'updated:', vm.tasks[ index ] );
+        /// - end test - ///
+        // $http call to toggle
+        $http({
+            method: 'PUT',
+            url: '/tasks',
+            data: vm.tasks[ index ]
+        }).then( function( response ){
+            console.log( 'back from server with:', response );
+            vm.getTasks();
+        }).catch( function ( err ){
+            console.log( 'error updating task:', err );
+        }) // end $http
+    } // end task click
+
+    //init
+    vm.getTasks();
 }) // end TasksController
